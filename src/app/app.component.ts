dce7bada1175be1c5addc0d153e56ca965aa4c80
sumit kumar <sumit.kumar271773@gmail.com>
2024-07-21 23:07:33 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink,RouterOutlet } from '@angular/router';
-import { WebpageComponent } from './webpage/webpage.component';
 import { DevilComponent } from './devil/devil.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,7 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WebpageComponent,RouterLink,DevilComponent,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,RouterLink,DevilComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
